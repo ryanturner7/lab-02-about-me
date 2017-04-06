a7ec9cb32c1ea663f08f46a6ebe34d1a144b50c2
Ryan Turner <ryanthepit@gmail.com>
2017-04-05 21:01:50 -0700

    About me lab extended

--- app.js
+++ app.js
@@ -1,36 +1,75 @@
 'use strict';
+
+// TODO: I want to prompt the user to guess the number 12
+// and get four chances
+var correctNumber = 5;
+var notDone = true;
+var guess;
+for (var attempts = 4; attempts > 0 && notDone; attempts--){
+    guess = prompt('How many miles did I walk yesterday?');//get user input
+    guess = parseInt(guess); //convert user input to number
+    console.log(guess);
+    if (guess == correctNumber){
+      alert('You guessed the correct number!');
+      notDone = false;
+    } else if (guess > 5 ){
+        alert('Too high, guess again.');
+      } else if (guess < 5 ){
+        alert('Too low, guess again.');
+    } else if (guess === 1){
+        alert('You are out of guesses!');
+      } else {
+      alert('Guess again.');
+    }
+}
+var correctAnswer = ['Supra', 'Eclipse', 'M3', 'SLS AMG'];
+var notDone = true;
+var guess;
+for (var attempts = 6; attempts > 0 && notDone; attempts--){
+  var userResponse = 'You are incorrect!'
+  guess = prompt('Can you guess one of my favorite cars?');
+  for (var i=0; i < correctAnswer.length && notDone; i++ ){
+    if(guess.trim().toLowerCase() === correctAnswer[i].trim().toLowerCase()){
+      userResponse = 'You are correct';
+      notDone = false;
+    } else if (attempts === 1){
+      userResponse = 'You are out of guesses!';
+    }
+  }
+  alert(userResponse)
+  }
 var vegan = prompt('Do you think Ryan is vegan?');
 console.log('My answer is ', vegan);
 if (vegan.trim().toLowerCase() == 'no') {
-    alert('You are correct!');
+  alert('You are correct!');
 } else {
-    alert('You dont know his life!');
+  alert('You dont know his life!');
 }
 var siblings = prompt('Does Ryan have siblings?');
 console.log('My answer is', siblings);
 if (siblings.trim().toLowerCase() == 'yes') {
-    alert('Close but no cigar!');
+  alert('Close but no cigar!');
 } else {
-    alert('You are correct!');
+  alert('You are correct!');
 }
 var children = prompt('Does Ryan have children?');
 console.log('My answer is', children);
 if (children.trim().toLowerCase() == 'yes') {
-    alert('You dont know him well!');
+  alert('You dont know him well!');
 } else {
-    alert('You remembered!');
+  alert('You remembered!');
 }
 var music = prompt('Does Ryan love music?');
 console.log('my answer is', music);
 if (music.trim().toLowerCase() == 'yes') {
-    alert('In fact, music is his life!');
+  alert('In fact, music is his life!');
 } else {
-    alert('Everybody loves music!');
+  alert('Everybody loves music!');
 }
 var coding = prompt('Is Ryan sufficient at coding?');
 console.log('my answer is', coding);
 if (coding.trim().toLowerCase() == 'yes') {
-    alert('You are absolutely correct.');
+  alert('You are absolutely correct.');
 } else {
-    alert('Incorrect, better than you!');
+  alert('Incorrect, better than you!');
 }
